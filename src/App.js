aefcdb389df32dbb45ed56aefd2642a0cdde42e6
caoyangyang <2451772768@qq.com>
2018-04-01 11:18:11 +0800

    t

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: cyy 
+ * @Date: 2018-04-01 11:17:52 
+ * @Last Modified by:   cyy 
+ * @Last Modified time: 2018-04-01 11:17:52 
+ */
 import React, { Component } from 'react';
 import CircleProgress from "../src/componets/circleProgress";
 import './App.css';
