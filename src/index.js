9f3fd3162247f81e81079b6ffa9e7632008b277a
caoyangyang <2451772768@qq.com>
2018-04-28 17:33:55 +0800

    上传案例图片

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(<div>
-    <CircleProgress percent={33} />
-    <CanCirPro percent={88} />
-    <SvgCirPro percent={100}/>
+    <CircleProgress percent={33} radius={50} borderWidth={10} textStyle={{ fontSize: 23, color: "#fa9a22", textAlign: 'center' }} color='#61ebff'  />
+    <CanCirPro percent={88} radius={50} borderWidth={10} textStyle={{ fontSize: 23, color: "#fa9a22", textAlign: 'center' }} startcolor='#ffd460' centercolor='#fcc241' endColor='#f79d00' />
+    <SvgCirPro percent={100} radius={50} borderWidth={10} textStyle={{ fontSize: 23, color: "#58c3fd", textAlign: 'center' }} startcolor='#61ebff' centercolor='#3fa2f7' endColor='#4f9cfc' />
 </div>, document.getElementById('root'));
 
